268d961abb018414dbdadd49dfd2ea05e118339a
nicholasgswan <nicholasgswan@gmail.com>
2026-04-11 18:27:29 -0700

    codecrafters submit [skip ci]

--- app/main.go
+++ app/main.go
@@ -41,7 +41,7 @@ func main() {
 		ind := 0
 		for ind > -1 {
 			ind = bytes.IndexByte(data, sep)
-			//fmt.Println("message from connection: ", string(data[:ind]))
+			fmt.Println("message from connection: ", string(data[:ind]))
 			if ind > -1 && string(data[:ind]) == "PING" {
 				conn.Write([]byte("+PONG\r\n"))
 			}
