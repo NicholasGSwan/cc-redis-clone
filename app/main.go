9834122aecaa210e9aa2d2910f3f6e233194af6e
nicholasgswan <nicholasgswan@gmail.com>
2026-04-14 13:12:02 -0700

    codecrafters submit [skip ci]

--- app/main.go
+++ app/main.go
@@ -52,6 +52,7 @@ func sendResponse(l net.Listener) {
 		if v == "PING" {
 			sendPong(conn)
 		} else {
+			conn.Write('u+0024')
 			conn.Write([]byte(strconv.Itoa(len(v))))
 			conn.Write(rn)
 			conn.Write([]byte(v))
