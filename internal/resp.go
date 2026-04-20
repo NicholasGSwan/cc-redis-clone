7bae0c0b1bdef53cd6015c59f9030b65def07aa2
nicholasgswan <nicholasgswan@gmail.com>
2026-04-20 14:33:05 -0700

    codecrafters submit [skip ci]

--- internal/resp.go
+++ internal/resp.go
@@ -65,7 +65,7 @@ func parseNextString(datap *[]byte) string {
 	switch strings.ToLower(s) {
 	case ECHO:
 		s = parseNextString(&data)
-		s = buildRespString(s)
+
 	case "set":
 		s = parseSetCommand(&data)
 	case "get":
