c320bb9751e8b7c6d28ad1ac27039e5e61b61cac
nicholasgswan <nicholasgswan@gmail.com>
2026-04-20 14:41:01 -0700

    codecrafters submit [skip ci]

--- internal/resp.go
+++ internal/resp.go
@@ -22,7 +22,7 @@ func Parse(datap *[]byte) []string {
 	fmt.Println("begin parsing")
 	fmt.Println("len of data: ", len(data))
 	parsed := make([]string, 0)
-	fmt.Printf("The first byte is: $s", string(data[0]))
+	fmt.Printf("The first byte is: %c", data[0])
 	switch data[0] {
 	case '*':
 		ind := bytes.Index(data, sep)
