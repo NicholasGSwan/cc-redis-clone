5555d7eb4731ea52f7badd421174149d0293eddf
nicholasgswan <nicholasgswan@gmail.com>
2026-04-20 14:20:27 -0700

    codecrafters submit [skip ci]

--- app/main.go
+++ app/main.go
@@ -65,9 +65,11 @@ func sendResponse(conn net.Conn) {
 				fmt.Println("the number of values in returned valArr: ", len(valArr))
 
 				for _, val := range valArr {
-					fmt.Println("curr val: ", val)
-					conn.Write([]byte(val))
-					conn.Write(rn)
+					if val != "" {
+						conn.Write([]byte(val))
+						conn.Write(rn)
+					}
+
 				}
 
 			}
